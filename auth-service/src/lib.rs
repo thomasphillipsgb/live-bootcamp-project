6942dd607714b933ffa73c4ba21681b57354f92f
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-04 17:33:06 +0100

    [S1][T5] feat: extracting direct server setup to library via Application struct.
    chore: scaffold auth service api tests

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+
+use axum::{response::Html, routing::get, serve::Serve, Router};
+use tower_http::services::ServeDir;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+        .nest_service("/", ServeDir::new("assets"))
+        .route("/hello", get(hello_handler));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        Ok(Self { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("<h1>Hello, Rustaceans!</h1>")
+}
