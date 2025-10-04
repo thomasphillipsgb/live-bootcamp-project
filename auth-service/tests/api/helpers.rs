9230e2699ea158388153743ea8ddb604078094a0
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-04 19:47:49 +0100

    [S1][T6] feat: adding tests and filling TestApp with helper methods.

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,68 @@
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0").await.expect("Failed to build application");
+        let address = format!("http://{}", app.address);
+
+        let _ = tokio::spawn(app.run());
+
+        let http_client = reqwest::Client::new();
+
+        TestApp { address, http_client }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&self.address)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_signup(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_login(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_logout(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_verify_2fa(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-2fa", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_verify_token(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-token", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+}
