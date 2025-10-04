511d6cf3aa26af51a9f340b64de00b3a398df6ba
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-04 20:55:16 +0100

    feat: move tests into their own files

---
+++ auth-service/tests/api/routes/mod.rs
@@ -0,0 +1,15 @@
+mod login;
+mod logout;
+mod root;
+mod signup;
+mod verify_2fa;
+mod verify_token;
+
+// re-export items from sub-modules
+pub use login::*;
+pub use logout::*;
+pub use root::*;
+pub use signup::*;
+pub use verify_2fa::*;
+pub use verify_token::*;
+pub use verify_token::*;
