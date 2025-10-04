b9c25edca9720329e611cdda239d4d9e985edaf9
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-04 18:17:05 +0100

    feat: add reqwest dependency and update other dependencies

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -4,19 +4,19 @@ use axum::{response::Html, routing::get, serve::Serve, Router};
 use tower_http::services::ServeDir;
 
 pub struct Application {
-    server: Serve<Router, Router>,
+    server: Serve<tokio::net::TcpListener, Router, Router>,
     pub address: String
 }
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
-        .nest_service("/", ServeDir::new("assets"))
+        .fallback_service(ServeDir::new("assets"))
         .route("/hello", get(hello_handler));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
-        let server = axum::serve(listener, router);
+        let server: Serve<tokio::net::TcpListener, Router, Router> = axum::serve(listener, router);
 
         Ok(Self { server, address })
     }
