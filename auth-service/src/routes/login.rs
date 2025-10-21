dd7a088c33a6396ce2797a66093f6564fd9d7b27
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-21 23:37:47 +0100

    update login route for 442 case

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,4 +1,11 @@
-use axum::{http, response::IntoResponse};
-pub async fn login_handler() -> impl IntoResponse {
+use axum::{http, response::IntoResponse, Json};
+
+#[derive(serde::Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+pub async fn login_handler(Json(payload): Json<LoginRequest>) -> impl IntoResponse {
     http::StatusCode::OK
 }
