65875e1aaa4123de8691a60201392b7caf5d8932
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-21 23:44:20 +0100

    implement 400 case and test

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,11 +1,26 @@
 use axum::{http, response::IntoResponse, Json};
 
+use crate::domain::{
+    models::{Email, Password},
+    AuthAPIError,
+};
+
 #[derive(serde::Deserialize)]
 pub struct LoginRequest {
     pub email: String,
     pub password: String,
 }
 
-pub async fn login_handler(Json(payload): Json<LoginRequest>) -> impl IntoResponse {
-    http::StatusCode::OK
+pub async fn login_handler(
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
+
+    let (email, password) = match (Email::new(email), Password::new(password)) {
+        (Ok(email), Ok(password)) => (email, password),
+        _ => return Err(AuthAPIError::InvalidCredentials),
+    };
+
+    Ok(http::StatusCode::OK)
 }
--- auth-service/tests/api/routes/login.rs
+++ auth-service/tests/api/routes/login.rs
@@ -8,3 +8,17 @@ async fn login_returns_422_if_malformed_request() {
 
     assert_eq!(response.status(), 422);
 }
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // Call the log-in route with invalid credentials and assert that a
+    // 400 HTTP status code is returned along with the appropriate error message.
+    let app = TestApp::new().await;
+
+    let response = app.post_login(&serde_json::json!({
+        "email": "invalid-email",
+        "password": "short"
+    })).await;
+
+    assert_eq!(response.status(), 400);
+}
