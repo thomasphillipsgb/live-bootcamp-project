2c29a3e18dad8be4e3707b67cc5a3ec78e1a4a9d
Maxgamerboy1 <phillipstom60@gmail.com>
2025-10-31 20:35:07 +0000

    feat(verify_2fa): implement 2FA verification handler and update related tests

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,5 +1,24 @@
-use axum::{http, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
 
-pub async fn verify_2fa_handler() -> impl IntoResponse {
-    http::StatusCode::OK
+use crate::{
+    domain::models::Email,
+    services::{LoginAttemptId, TwoFACode},
+};
+
+pub async fn verify_2fa_handler(Json(request): Json<Verify2FARequest>) -> impl IntoResponse {
+    match (
+        Email::new(request.email),
+        LoginAttemptId::new(request.login_attempt_id),
+        TwoFACode::new(request.two_fa_code),
+    ) {
+        (Ok(_email), Ok(_login_attempt_id), Ok(_two_fa_code)) => StatusCode::OK.into_response(),
+        _ => StatusCode::BAD_REQUEST.into_response(),
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct Verify2FARequest {
+    pub email: String,
+    pub login_attempt_id: String,
+    pub two_fa_code: String,
 }
--- auth-service/tests/api/routes/verify_2fa.rs
+++ auth-service/tests/api/routes/verify_2fa.rs
@@ -1,10 +1,25 @@
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn verify_2fa_returns_200() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_verify_2fa().await;
+    let response = app.post_verify_2fa(&()).await;
 
-    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.status().as_u16(), 422);
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let response = app
+        .post_verify_2fa(&serde_json::json!({
+            "email": "userm",
+            "login_attempt_id": "string",
+            "two_fa_code": "string"
+        }))
+        .await;
+
+    assert_eq!(response.status().as_u16(), 400);
 }
