0eb9ca39854b269a182567a4413c0a0c50fc98f4
Maxgamerboy1 <phillipstom60@gmail.com>
2025-11-07 22:20:59 +0000

    refactor: reorganize volume declaration in Docker Compose file

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -2,15 +2,18 @@ use std::sync::Arc;
 
 use auth_service::{
     domain::mock_email_client::MockEmailClient,
+    get_postgres_pool,
     services::{
         data_stores::{
-            hashset_banned_store::HashsetBannedTokenStore, HashMapUserStore, HashmapTwoFACodeStore,
+            hashset_banned_store::HashsetBannedTokenStore, postgres_user_store::PostgresUserStore,
+            HashMapUserStore, HashmapTwoFACodeStore,
         },
         BannedTokenStore,
     },
-    utils::constants::test,
+    utils::constants::{test, DATABASE_URL},
     Application,
 };
+use sqlx::{postgres::PgPoolOptions, Executor, PgPool};
 use uuid::Uuid;
 
 pub struct TestApp {
@@ -19,12 +22,14 @@ pub struct TestApp {
     pub http_client: reqwest::Client,
     pub banned_token_store: Arc<tokio::sync::RwLock<dyn BannedTokenStore>>,
     pub two_fa_code_store: Arc<tokio::sync::RwLock<HashmapTwoFACodeStore>>,
-    pub user_store: Arc<tokio::sync::RwLock<HashMapUserStore>>,
+    pub user_store: Arc<tokio::sync::RwLock<PostgresUserStore>>,
 }
 
 impl TestApp {
     pub async fn new() -> Self {
-        let user_store = Arc::new(tokio::sync::RwLock::new(HashMapUserStore::new()));
+        let pg_pool = configure_postgresql().await;
+
+        let user_store = Arc::new(tokio::sync::RwLock::new(PostgresUserStore::new(pg_pool)));
         let banned_token_store = Arc::new(tokio::sync::RwLock::new(HashsetBannedTokenStore::new()));
         let two_fa_code_store = Arc::new(tokio::sync::RwLock::new(HashmapTwoFACodeStore::new()));
         let email_client = Arc::new(tokio::sync::RwLock::new(MockEmailClient {}));
@@ -127,4 +132,48 @@ impl TestApp {
 
 pub fn get_random_email() -> String {
     format!("{}@example.com", Uuid::new_v4())
+}
+
+async fn configure_postgresql() -> PgPool {
+    let postgresql_conn_url = DATABASE_URL.to_owned();
+
+    // We are creating a new database for each test case, and we need to ensure each database has a unique name!
+    let db_name = Uuid::new_v4().to_string();
+
+    configure_database(&postgresql_conn_url, &db_name).await;
+
+    let postgresql_conn_url_with_db = format!("{}/{}", postgresql_conn_url, db_name);
+
+    // Create a new connection pool and return it
+    get_postgres_pool(&postgresql_conn_url_with_db)
+        .await
+        .expect("Failed to create Postgres connection pool!")
+}
+
+async fn configure_database(db_conn_string: &str, db_name: &str) {
+    // Create database connection
+    let connection = PgPoolOptions::new()
+        .connect(db_conn_string)
+        .await
+        .expect("Failed to create Postgres connection pool.");
+
+    // Create a new database
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, db_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Connect to new database
+    let db_conn_string = format!("{}/{}", db_conn_string, db_name);
+
+    let connection = PgPoolOptions::new()
+        .connect(&db_conn_string)
+        .await
+        .expect("Failed to create Postgres connection pool.");
+
+    // Run migrations against new database
+    sqlx::migrate!()
+        .run(&connection)
+        .await
+        .expect("Failed to migrate the database");
 }
